2a2ff57968a597455e6665b190522b11ac942d38
root <czwen97@gmail.com>
2021-11-30 02:06:46 +0800

    change local ip to vm ip

--- src/test/java/mycompany/app/AppTest.java
+++ src/test/java/mycompany/app/AppTest.java
@@ -23,7 +23,7 @@ public class AppTest
 {
 	WebDriver driver; 
 	WebDriverWait wait; 
-	String url = "192.168.10.131";
+	String url = "http://116.89.35.68";
 	String validEmail = "user@example.com";
 	String validPassword = "password1234";
 	String invalidEmail = "none@example.com";
